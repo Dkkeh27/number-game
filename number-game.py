efd058be926ed38c6a81e2072dde8e177639efbd
Kim JiMin <wlalsdldy1@naver.com>
2023-12-07 21:56:59 +0900

    design commit3
    add frame_lavel easy normal hard

--- number-game.py
+++ number-game.py
@@ -29,6 +29,28 @@ label_info = tk.Label(frame_main, text="숫자 맞추기 게임에 오신 것을
                         width=0, height=0, fg="blue", font=("Arial", 18))
 label_info.place(x=130, y=15)
 
+# 서브 프레임 생성 (in 메인프레임)
+frame_sub = tk.Frame(frame_main, relief="solid", bd=0)
+frame_sub.place(x=10, y=60)
+
+# 난이도 선택 프레임, 버튼 생성 (in 서브프레임)
+frame_level  = tk.LabelFrame(frame_sub, text="난이도")
+
+mode_var     = tk.IntVar() # 라디오 버튼 값
+radio_easy   = tk.Radiobutton(frame_level, text="쉬움",   width=6, value=1, variable=mode_var) # 쉬움 모드 값   == 1
+radio_normal = tk.Radiobutton(frame_level, text="보통",   width=6, value=2, variable=mode_var) # 보통 모드 값   == 2
+radio_hard   = tk.Radiobutton(frame_level, text="어려움", width=6, value=3, variable=mode_var) # 어려움 모드 값 == 3
+
+radio_easy.deselect()           #쉬움   체크해제
+radio_normal.select()           #보통   체크
+radio_hard.deselect()           #어려움 체크해제
+
+radio_easy.pack  (side="left")  #왼쪽 정렬
+radio_normal.pack(side="left")
+radio_hard.pack  (side="left")
+
+# 행렬 배치
+frame_level.grid (row=1, column=1)                                  # 1행   1열
 
 
 root.mainloop()
